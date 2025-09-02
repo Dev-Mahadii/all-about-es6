7e0a0e9d8e5f0d6992cc602e58f451d5eff5741b
Dev Mahadi <mahadihasan.dev24@gmail.com>
2025-09-02 22:41:47 +0600

    all about ecma script

---
+++ ecma-script.js
@@ -0,0 +1,21 @@
+// Template String
+
+const number1 = 5;
+const number2 = 10;
+
+const sum = `The sum of number1: ${number1} and number2: ${number2} is ${number1+number2}`
+
+let poem = `amar poran jaha chay
+tumi tai tumi tai tmi tai go
+amar o poran jaha chay`
+
+
+console.log(sum);
+console.log(poem);
+
+
+// Arrow Function
+
+
+
+
